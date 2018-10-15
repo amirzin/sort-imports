f8db12823611d8dda5cb0922297a422df44f4e22
Мирзин Артур Робертович <artur.mirzin@akbarsdigital.ru>
2018-10-16 02:13:42 +0300

    Added simple sorting

--- src/modules/sort.ts
+++ src/modules/sort.ts
@@ -1,6 +1,25 @@
 import * as t from '@babel/types';
 
+import { isAbsoluteImport, isRelativeImport, isStyleImport } from './utils';
+
 export const sort = (imports: Array<t.ImportDeclaration>) => {
     // sort here
-    return imports;
+    const absoluteImports: Array<t.ImportDeclaration> = [];
+    const relativeImports: Array<t.ImportDeclaration> = [];
+    const stylesImports: Array<t.ImportDeclaration> = [];
+
+    imports.forEach((importDeclaration) => {
+        if (isAbsoluteImport(importDeclaration)) {
+            absoluteImports.push(importDeclaration);
+        }
+
+        if (isRelativeImport(importDeclaration)) {
+            relativeImports.push(importDeclaration);
+        }
+    });
+
+    return [
+        ...absoluteImports,
+        ...relativeImports,
+    ];
 };
--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,11 +1,13 @@
-export const isAbsoluteImport = () => {
+import * as t from '@babel/types';
 
+export const isAbsoluteImport = (importDeclaration: t.ImportDeclaration): boolean => {
+    return !isRelativeImport(importDeclaration);
 };
 
-export const isRelativeImport = () => {
-
+export const isRelativeImport = (importDeclaration: t.ImportDeclaration): boolean => {
+    return importDeclaration.source.value.indexOf(".") === 0;
 };
 
-export const isStyleImport = () => {
+export const isStyleImport = (importDeclaration: t.ImportDeclaration) => {
 
 };
