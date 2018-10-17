48bfcce861a8877d656bf3aa0c62500a34a87e06
Artur Mirzin <whturlkinat@gmail.com>
2018-10-17 03:41:38 +0300

    Renamed package and command

--- src/extension.js
+++ src/extension.js
@@ -8,11 +8,11 @@ import * as babelParser from '@babel/parser';
 export function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "react-imports-sorter" is now active!');
+    console.log('Congratulations, your extension "sort-imports" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.sortImports', () => {
         // The code you place here will be executed every time your command is executed
         //writeCode('123');
         const x = babelParser.parse('import fs from "fs"');
